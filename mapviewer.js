23895c227189c662e839278880db6bbdb5452acb
slaren <ddevesa@gmail.com>
2013-03-18 22:37:03 +0100

    update

--- mapviewer.js
+++ mapviewer.js
@@ -1,4 +1,13 @@
 (function() {
+	function sformat(str) {
+		var args = arguments;
+		return str.replace(/{(\d+)}/g, function(match, number) { 
+			return typeof args[number] != 'undefined'
+				? args[number]
+				: match;
+			});
+	};
+
 	var tribe_colors = []
 	function get_tribe_color(tribe_id) {
 		if (!tribe_colors[tribe_id]) {
@@ -7,15 +16,76 @@
 		return tribe_colors[tribe_id];
 	}
 
-	var scale = 0.1;
+
+	var cur_trans = [0, 0];
+	var cur_scale = 0.1;
+	var svg;
 	var content;
+	var map_data;
+	function redraw_map() {
+		// console.log("redraw_map");
+		var trans = d3.event.translate;
+		var scale = d3.event.scale;
+		var target = content;
+		if (scale != cur_scale)
+			target = content.transition();
+
+		target.attr("transform", "translate(" + trans + ")" + " scale(" + scale + ")");
+
+		cur_scale = scale;
+		cur_trans = trans;
+	}
+
+	function update_cursor() {
+		var pos = d3.mouse(this);
+		var x = pos[0] / 4;
+		var y = pos[1];
+		var text = x.toFixed() + " " + y.toFixed();
+		cursor_text.text(text);
+	}
+
+	function get_player_name(player_id) {
+		return map_data.Players.indexOf(function(d) { return d.playerId == player_id})
+	}
+
+	function format_city(city) {
+		return sformat("{1} / {2} / {3} / {4}", city.name, city.level, get_player_name(city.playerId), "");
+	}
+
 	function init_map(data) {
-		var svg = d3.select("svg");
-		content = svg.append("g")
-			.attr("x", 0)
-			.attr("y", 0)
-			.attr("transform", "scale("+scale+")");
+		map_data = data;
+		svg = d3.select("svg");
 		
+		var map_width = 13600;
+		var map_height = 6200;
+		var svg_width = parseInt(svg.style("width"));
+		var svg_height = parseInt(svg.style("height"));
+
+		var zoom = d3.behavior.zoom()
+			.translate(cur_trans)
+    		.scale(cur_scale)
+    		.scaleExtent([Math.min(svg_width/map_width, svg_height/map_height), 1])
+    		.on("zoom", redraw_map);
+
+		zoom_container = svg.append("g")
+			.call(zoom)
+
+		// map content
+		content = zoom_container.append("g")
+			.attr("transform", "scale("+cur_scale+")")
+			.on("mousemove", update_cursor);
+
+
+		content.append("rect")
+			.attr("width", map_width)
+			.attr("height", map_height)
+			.attr("fill", "white");
+
+		content.append("image")
+			.attr("width", map_width)
+			.attr("height", map_height)
+			.attr("xlink:href", "influence_bitmap_small.png");
+
 		// DATA JOIN
 		// Join new data with old elements, if any.
 		var cities = content.selectAll(".city")
@@ -27,26 +97,38 @@
 
 		// ENTER
 		// Create new elements as needed.
-		
+		cities.enter().append("polygon")
+			.attr("points", "-7,0 0,-4 7,0 0,4")
+			.attr("stroke", "black")
+			.attr("transform", function(d, i) { return "translate(" + (d.x*4) + "," + (d.y*1) + ")" /* + " scale(" + Math.min(2, Math.max(1, (d.value/70))) + ")"*/; })
+			.attr("fill", function(d, i) { return get_tribe_color(d.tribeId); })
+			.on("mouseover", function(d) { info_text.text(format_city(d)); })
+			.on("mouseout", function(d) { info_text.text("") })
+/*
 		cities.enter().append("use")
 			.attr("xlink:href", "#sym-city")
-			.attr("transform", function(d, i) { return "translate(" + (d.x*4) + "," + (d.y*1) + ")" + " scale(" + (d.value/50) + ")"; })
-			//.attr("x", function(d, i) { return d.x * 4 * scale; })
-			//.attr("y", function(d, i) { return d.y * 1 * scale; })
-			//.attr("transform", function(d, i) { return "translate(" + (d.x*4*scale) + "," + (d.y*1*scale) + ")"; })
+			.attr("transform", function(d, i) { return "translate(" + (d.x*4) + "," + (d.y*1) + ")" + " scale(" + (d.value/70) + ")"; })
 			.attr("fill", function(d, i) { return get_tribe_color(d.tribeId); })
+*/
+/*
+		cities.enter().append("circle")
+			// .attr("cx", function(d, i) { return d.x * 4; })
+			// .attr("cy", function(d, i) { return d.y * 1; })
+			.attr("r", 10)
+			.attr("transform", function(d, i) { return "translate(" + (d.x*4) + "," + (d.y*1) + ")" + " scale(" + (d.value/70) + ")"; })
+			.attr("stroke", "black")
+			.attr("fill", function(d, i) { return get_tribe_color(d.tribeId); })
+*/
+		// static stuff
+		cursor_text = zoom_container.append("text")
+			.attr("x", 10)
+			.attr("y", 20)
+			.attr("fill", "black")
 
-		// ENTER + UPDATE
-		// Appending to the enter selection expands the update selection to include
-		// entering elements; so, operations on the update selection after appending to
-		// the enter selection will apply to both entering and updating nodes.
-		//cities.text(function(d) { return d; });
-
-		// EXIT
-		// Remove old elements as needed.
-		cities.exit().remove();
-
-		console.log("done")
+		info_text = zoom_container.append("text")
+			.attr("x", 10)
+			.attr("y", 40)
+			.attr("fill", "black")
 	}
 
 	d3.json("map.json", function(error, data) {
