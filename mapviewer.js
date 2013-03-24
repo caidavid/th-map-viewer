d17e82b14837751a0fbdae03bbeb61d4e354dbc6
slaren <ddevesa@gmail.com>
2013-03-24 23:31:57 +0100

    update

--- mapviewer.js
+++ mapviewer.js
@@ -1,10 +1,10 @@
 (function() {
 	function sformat(str) {
 		var args = arguments;
-		return str.replace(/{(\d+)}/g, function(match, number) { 
+		return str.replace(/{(\d+)}/g, function(match, number) {
 			return typeof args[number] != 'undefined'
-			? args[number]
-			: match;
+				? args[number]
+				: match;
 		});
 	};
 
@@ -18,18 +18,18 @@
 		}
 	}
 
-	var tribe_colors = { 0: "#fff" }
+	var tribe_colors = { 0: "#fff" };
 	function get_tribe_color(tribe_id) {
 		return get_random_color(tribe_colors, tribe_id);
 	}
 
-	var color_tribes = { }
+	var color_tribes = {};
 	function get_tribe_by_color(r, g, b) {
 		var col = "#" + ("000000" + (r << 16 | g << 8 | b).toString(16)).slice(-6);
 		return color_tribes[col];
 	}
 
-	var player_colors = {}
+	var player_colors = {};
 	function get_player_color(player_id) {
 		return get_random_color(player_colors, player_id);
 	}
@@ -56,9 +56,8 @@
 	var ymin, ymax;
 
 	function on_zoom() {
-		var trans = zoom.translate().slice()
+		var trans = zoom.translate().slice();
 		var scale = zoom.scale();
-		// console.log("on_zoom", cur_scale, cur_trans, "=>", scale, trans)
 
 		if (scale != cur_scale) {
 			transition_zoom(trans, scale, 250);
@@ -73,10 +72,9 @@
 	function transition_zoom(trans, scale, duration) {
 		var t1 = cur_trans.slice();
 		var s1 = cur_scale;
-		
+
 		var t2 = trans.slice();
 		var s2 = scale;
-		// console.log("transition:", "s:", s1, "=>", s2, "tr:", t1,"=>", t2);
 
 		d3.transition()
 			.duration(duration)
@@ -119,6 +117,7 @@
 	var mouse_raw;
 	var mouse_x = 0, mouse_y = 0;
 	var last_frame_time = 0;
+
 	function update_cursor_text() {
 		var x = Math.max(0, Math.min(tiles_width, Math.floor(mouse_x / 4)));
 		var y = Math.max(0, Math.min(tiles_height, Math.floor(mouse_y)));
@@ -138,22 +137,22 @@
 			var dx = ox - x, dy = oy - y;
 			return dx * dx + dy * dy;
 		}
-		
+
 		min_dist_sq = 10 * 10;
+
+		for (var i = 0; i < frame_objects.strongholds.length; ++i) {
+			var obj = frame_objects.strongholds[i];
+			var dist_sq = get_dist_sq(obj.x * 4, obj.y);
+			if (dist_sq < min_dist_sq) {
+				return [ "stronghold", obj ];
+			}
+		}
 
 		for (var i = 0; i < frame_objects.troops.length; ++i) {
 			var obj = frame_objects.troops[i];
 			var dist_sq = get_dist_sq(obj.x * 4, obj.y);
 			if (dist_sq < min_dist_sq) {
 				return [ "troop", obj ];
-			}
-		}
-
-		for (var i = 0; i < frame_objects.barbarians.length; ++i) {
-			var obj = frame_objects.barbarians[i];
-			var dist_sq = get_dist_sq(obj.x * 4, obj.y);
-			if (dist_sq < min_dist_sq) {
-				return [ "barbarian", obj ];
 			}
 		}
 
@@ -164,17 +163,10 @@
 				return [ "city", obj ];
 			}
 		}
-
-		for (var i = 0; i < frame_objects.strongholds.length; ++i) {
-			var obj = frame_objects.strongholds[i];
-			var dist_sq = get_dist_sq(obj.x * 4, obj.y);
-			if (dist_sq < min_dist_sq) {
-				return [ "stronghold", obj ];
-			}
-		}
 	}
 
-	var mouseover_timer;	
+	var mouseover_timer;
+
 	function on_canvas_mousemove() {
 		// update mouse coords
 		var pos = d3.mouse(this);
@@ -183,8 +175,8 @@
 		mouse_y = (pos[1] - cur_trans[1]) / cur_scale;
 		update_cursor_text();
 
+		// show mouseover object info
 		clearTimeout(mouseover_timer);
-
 		var obj = get_object_by_location(mouse_x, mouse_y);
 		canvas.style("cursor", obj ? "pointer" : "auto")
 		if (obj) {
@@ -214,7 +206,6 @@
 	}
 
 	function update_mouseover_influence() {
-		// show mouseover object info
 		var img_data = canvas_ctx.getImageData(mouse_raw[0], mouse_raw[1], 1, 1).data;
 		var tribeid = get_tribe_by_color(img_data[0], img_data[1], img_data[2]);
 		if (tribeid) {
@@ -257,9 +248,15 @@
 	}
 
 	function calc_selection_center() {
-		var sum_x = 0, sum_y = 0;
-		_(selected_objects).each(function(c) { sum_x += c.x; sum_y += c.y; })
-		return [sum_x / selected_objects.length, sum_y / selected_objects.length];
+		var min_x = tiles_width, max_x  = 0;
+		var min_y = tiles_height, max_y = 0;
+		_(selected_objects).each(function(c) {
+			min_x = Math.min(min_x, c.x);
+			min_y = Math.min(min_y, c.y);
+			max_x = Math.max(max_x, c.x);
+			max_y = Math.max(max_y, c.y);
+		})
+		return [(max_x + min_x) / 2, (max_y + min_y) / 2];
 	}
 
 	function calc_selection_radius() {
@@ -273,7 +270,6 @@
 	}
 
 	function is_selected(obj) {
-		// console.log(selected_objects);
 		return selected_objects.indexOf(obj) != -1;
 	}
 
@@ -281,7 +277,7 @@
 		canvas.node().focus();
 
 		var obj = get_object_by_location(mouse_x, mouse_y);
-		
+
 		if (obj) {
 			set_selection(obj[1]);
 			center_map_tile(obj[1].x, obj[1].y);
@@ -304,7 +300,7 @@
 	}
 
 	function show_city_info(city) {
-		return sformat("City: {1} / Level {2} / Player: {3}{4}", city.name, city.level, get_player(city.playerId).name, city.tribeId != 0 && " (" + get_tribe(city.tribeId).name + ")" || "");
+		return sformat("City: {1} / Level {2} ({3} IP) / Player: {4}{5}", city.name, city.level, city.value, get_player(city.playerId).name, city.tribeId != 0 && " (" + get_tribe(city.tribeId).name + ")" || "");
 	}
 
 	function show_troop_info(troop) {
@@ -316,26 +312,64 @@
 		return sformat("Stronghold: {1} / Level {2} / {3}", sh.name, sh.level, sh.tribeId != 0 && get_tribe(sh.tribeId).name || "Unoccupied");
 	}
 
-	var search_input
-	var search_results
-	
+	var search_input;
+	var search_results;
+
 	function object_name_sort_comparer(x, y) {
 		return x.name < y.name ? -1 : 1;
 	}
 
-	function do_search() {
-		var q = search_input.property("value");
+	function filter_object(obj) {
+		if (obj.x) {
+			return obj;
+		}
+		else {
+			return map_data.Cities.filter(
+				function(c) {
+					return (obj.tribeId && (obj.tribeId == c.tribeId)) || (obj.playerId && (obj.playerId == c.playerId));
+				});
+		}
+	}
+
+	function select_object(obj) {
+		set_selection(filter_object(obj));
+		center_map_selection();
+	}
+
+	function do_search(q) {
 		var match_cities = [];
 		var match_players = [];
 		var match_tribes = [];
 		var match_strongholds = [];
-		
+
 		if(q && q.length > 0) {
 			q = q.toLowerCase();
-			match_cities = map_data.Cities.filter(function(c) { return c.name.toLowerCase().indexOf(q) != -1; }).sort();
-			match_players = map_data.Players.filter(function(c) { return c.name.toLowerCase().indexOf(q) != -1; });
-			match_tribes = map_data.Tribes.filter(function(c) { return c.name.toLowerCase().indexOf(q) != -1; });
-			match_strongholds = map_data.Strongholds.filter(function(c) { return c.name.toLowerCase().indexOf(q) != -1; });
+			var filter_fn = function(c) { return c.name.toLowerCase().indexOf(q) != -1; };
+			var exact_filter_fn = function(c) { return c.name.toLowerCase() == q; };
+
+			match_cities = map_data.Cities.filter(filter_fn);
+			match_players = map_data.Players.filter(filter_fn);
+			match_tribes = map_data.Tribes.filter(filter_fn);
+			match_strongholds = map_data.Strongholds.filter(filter_fn);
+
+			// if there is only one result or an exact result, jump to it
+			var num_results = match_cities.length + match_players.length + match_tribes.length + match_strongholds.length;
+			var exact = _(match_cities).find(exact_filter_fn) || _(match_players).find(exact_filter_fn) || _(match_tribes).find(exact_filter_fn) || _(match_strongholds).find(exact_filter_fn);
+			if (num_results == 1 || exact) {
+				var obj = exact || match_cities[0] || match_players[0] || match_tribes[0] || match_strongholds[0];
+				select_object(obj);
+				update_url();
+			}
+
+			// check if the user entered coordinates
+			var coords_regexp = /^\s*(\d+)[\s,]+(\d+)\s*$/;
+			var match = coords_regexp.exec(q);
+			if (match) {
+				var x = match[1], y = match[2];
+				if (x >= 0 && x <= tiles_width && y >= 0 && y <= tiles_height) {
+					center_map_tile(x, y);
+				}
+			}
 		}
 
 		// cities
@@ -346,10 +380,7 @@
 			.sort(object_name_sort_comparer)
 			.attr("class", "search_result city_search_result")
 			.text(function(d) { return "City: " + d.name; })
-			.on("click", function(d) { 
-				set_selection(d);
-				center_map_selection();
-			});
+			.on("click", select_object);
 
 		// players
 		search_results_players = search_results.selectAll(".player_search_result").data(match_players);
@@ -359,11 +390,7 @@
 			.sort(object_name_sort_comparer)
 			.attr("class", "search_result player_search_result")
 			.text(function(d) { return "Player: " + d.name; })
-			.on("click", function(d) { 
-				var cities = map_data.Cities.filter(function(c) { return c.playerId == d.playerId; });
-				set_selection(cities);
-				center_map_selection();
-			});
+			.on("click", select_object);
 
 		// tribes
 		search_results_tribes = search_results.selectAll(".tribe_search_result").data(match_tribes);
@@ -373,12 +400,7 @@
 			.sort(object_name_sort_comparer)
 			.attr("class", "search_result tribe_search_result")
 			.text(function(d) { return "Tribe: " + d.name; })
-			.on("click", function(d) { 
-				var cities = map_data.Cities.filter(function(c) { return c.tribeId == d.tribeId; });
-				set_selection(cities);
-				center_map_selection();
-			});
-
+			.on("click", select_object);
 
 		// strongholds
 		search_results_strongholds = search_results.selectAll(".stronghold_search_result").data(match_strongholds);
@@ -388,10 +410,7 @@
 			.sort(object_name_sort_comparer)
 			.attr("class", "search_result stronghold_search_result")
 			.text(function(d) { return "Stronghold: " + d.name; })
-			.on("click", function(d) { 
-				set_selection(d);
-				center_map_selection();
-			});
+			.on("click", select_object);
 	}
 
 	function center_map_tile(x, y, scale) {
@@ -410,7 +429,6 @@
 
 		center_map_tile(pos[0], pos[1], scale);
 	}
-
 
 	function get_min_zoom_scale() {
 		return Math.min(canvas_width / map_width, canvas_height / map_height)
@@ -446,15 +464,23 @@
 		search_input = d3.select("#search");
 		search_input.on("input", function() {
 			clearTimeout(do_search_timeout)
-			do_search_timeout = setTimeout(do_search, 50);
+			do_search_timeout = setTimeout(function() {
+				do_search(search_input.property("value"))
+			}, 50);
 		});
+
 		search_input.node().focus();
+		search_input.node().select();
 
 		search_results = d3.select("#search_results");
 
 		// canvas events
 		canvas.on("mousemove", on_canvas_mousemove);
 		canvas.on("click", on_canvas_click);
+
+		// load state
+		d3.select(window).on("hashchange", update_from_url);
+		update_from_url();
 
 		// first draw
 		draw();
@@ -468,13 +494,13 @@
 			draw();
 		}
 
-		d3.select("#filter_cities").on("change", function() { update_visibility("city", d3.event.target); })
-		d3.select("#filter_forests").on("change", function() { update_visibility("forest", d3.event.target); })
-		d3.select("#filter_strongholds").on("change", function() { update_visibility("stronghold", d3.event.target); })
-		d3.select("#filter_troops").on("change", function() { update_visibility("troop", d3.event.target); })
-		d3.select("#filter_barbarians").on("change", function() { update_visibility("barbarian", d3.event.target); })
-		d3.select("#filter_influence").on("change", function() { update_visibility("influence", d3.event.target); })
-		
+		d3.select("#filter_cities").on("change", function() { update_visibility("city", d3.event.target); });
+		d3.select("#filter_forests").on("change", function() { update_visibility("forest", d3.event.target); });
+		d3.select("#filter_strongholds").on("change", function() { update_visibility("stronghold", d3.event.target); });
+		d3.select("#filter_troops").on("change", function() { update_visibility("troop", d3.event.target); });
+		d3.select("#filter_barbarians").on("change", function() { update_visibility("barbarian", d3.event.target); });
+		d3.select("#filter_influence").on("change", function() { update_visibility("influence", d3.event.target); });
+
 		update_visibility("city", d3.select("#filter_cities").node());
 		update_visibility("forest", d3.select("#filter_forests").node());
 		update_visibility("stronghold", d3.select("#filter_strongholds").node());
@@ -527,21 +553,19 @@
 			.scale(cur_scale)
 			.scaleExtent([get_min_zoom_scale(), 1])
 			.on("zoom", on_zoom);
-		
+
 		canvas.call(zoom);
-		
+
 		canvas.on("keydown", function() {
 			var step = 100;
 			var x = cur_trans[0], y = cur_trans[1];
-			console.log(d3.event);
+			var scale = cur_scale;
 			switch (d3.event.keyCode) {
 				// +
 				case 187:
 					var pos = false;
 					var k = Math.log(cur_scale) / Math.LN2;
 					var scale = Math.pow(2, pos ? Math.ceil(k) - 1 : Math.floor(k) + 1);
-					set_zoom(cur_trans, scale);
-					draw();
 					break;
 
 				// -
@@ -549,10 +573,7 @@
 					var pos = true;
 					var k = Math.log(cur_scale) / Math.LN2;
 					var scale = Math.pow(2, pos ? Math.ceil(k) - 1 : Math.floor(k) + 1);
-					set_zoom(cur_trans, scale);
-					draw();
 					break;
-
 
 				// DOM_VK_LEFT	0x25 (37)	Left arrow.
 				case 37:
@@ -560,43 +581,39 @@
 					set_zoom([x, y], cur_scale);
 					draw();
 					break;
-				
+
 				// DOM_VK_UP	0x26 (38)	Up arrow.
 				case 38:
 					y += step * cur_scale;
-					set_zoom([x, y], cur_scale);
-					draw();
 					break;
-				
+
 				// DOM_VK_RIGHT	0x27 (39)	Right arrow.
 				case 39:
 					x -= step * cur_scale;
-					set_zoom([x, y], cur_scale);
-					draw();
 					break;
-				
+
 				// DOM_VK_DOWN	0x28 (40)	Down arrow.
 				case 40:
 					y -= step * cur_scale;
-					set_zoom([x, y], cur_scale);
-					draw();
 					break;
 			}
+
+			transition_zoom([x, y], scale);
 		});
-		
+
 		on_zoom();
 
 		d3.select(window).on("resize", function() {
 			resize_canvas();
 
-			var min_scale = get_min_zoom_scale() 
+			var min_scale = get_min_zoom_scale()
 			if(zoom.scale() < min_scale || zoom.scale() == zoom.scaleExtent()[0]) {
 				zoom.scale(min_scale);
 			}
-			zoom.scaleExtent([min_scale, 1])
+			zoom.scaleExtent([min_scale, 1]);
 
 			on_zoom();
-		})
+		});
 	}
 
 	var min_small_text_scale = 0.5;
@@ -605,7 +622,6 @@
 	var font_big = "bold 10pt Arial";
 	var font_normal = "10pt Arial";
 	var font_small = "8pt Arial";
-
 
 	function draw_outlined_text(text, x, y, w, outline, fill) {
 		canvas_ctx.fillStyle = outline;
@@ -691,7 +707,7 @@
 		// colored circunference
 		canvas_ctx.moveTo(x, y);
 		canvas_ctx.beginPath();
-		canvas_ctx.arc(x, y, rad, 0, 2 * Math.PI)
+		canvas_ctx.arc(x, y, rad, 0, 2 * Math.PI);
 		canvas_ctx.closePath();
 
 		canvas_ctx.strokeStyle = fill;
@@ -705,13 +721,13 @@
 
 			canvas_ctx.moveTo(x, y);
 			canvas_ctx.beginPath();
-			canvas_ctx.arc(x, y, rad + width, 0, 2 * Math.PI)
+			canvas_ctx.arc(x, y, rad + width, 0, 2 * Math.PI);
 			canvas_ctx.closePath();
 			canvas_ctx.stroke();
 
 			canvas_ctx.moveTo(x, y);
 			canvas_ctx.beginPath();
-			canvas_ctx.arc(x, y, rad - width, 0, 2 * Math.PI)
+			canvas_ctx.arc(x, y, rad - width, 0, 2 * Math.PI);
 			canvas_ctx.closePath();
 			canvas_ctx.stroke();
 		}
@@ -753,7 +769,7 @@
 					frame_objects.forests.push(forest);
 
 					canvas_ctx.beginPath();
-					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI)
+					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI);
 					canvas_ctx.closePath();
 
 					canvas_ctx.fillStyle = "green";
@@ -779,7 +795,7 @@
 					frame_objects.barbarians.push(barb);
 
 					canvas_ctx.beginPath();
-					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI)
+					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI);
 					canvas_ctx.closePath();
 
 					canvas_ctx.fillStyle = "blue";
@@ -805,12 +821,12 @@
 					frame_objects.troops.push(troop);
 
 					canvas_ctx.beginPath();
-					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI)
+					canvas_ctx.arc(x, y, 4, 0, 2 * Math.PI);
 					canvas_ctx.closePath();
 
 					canvas_ctx.fillStyle = get_tribe_color(troop.tribeId);;
 					canvas_ctx.fill();
-	
+
 					if (cur_scale > min_small_text_scale) {
 						canvas_ctx.lineWidth = 1;
 						canvas_ctx.strokeStyle = "black";
@@ -829,7 +845,7 @@
 
 				if (is_inside_viewport(x, y)) {
 					frame_objects.cities.push(city);
-				
+
 					if (cur_scale < min_normal_text_scale) {
 						var cw = 7;
 						var ch = 4;
@@ -923,7 +939,7 @@
 
 					// inner dot
 					canvas_ctx.beginPath();
-					canvas_ctx.arc(x, y, 5, 0, 2 * Math.PI)
+					canvas_ctx.arc(x, y, 5, 0, 2 * Math.PI);
 					canvas_ctx.closePath();
 
 					canvas_ctx.fillStyle = get_tribe_color(sh.tribeId);
@@ -935,7 +951,7 @@
 						canvas_ctx.stroke();
 					}
 
-					draw_circumference(x, y, 10 + sh.level * 5, 4, "black", get_tribe_color(sh.tribeId))
+					draw_circumference(x, y, 10 + sh.level * 5, 4, "black", get_tribe_color(sh.tribeId));
 				}
 			}
 		}
@@ -949,27 +965,39 @@
 			draw_text();
 		}
 		else {
-			clearTimeout(draw_text_timeout)
+			clearTimeout(draw_text_timeout);
 			draw_text_timeout = setTimeout(draw_text, 10);
 		}
 
 		var frame_time = window.performance.now() - start_time;
 		last_frame_time = frame_time;
 		update_cursor_text();
-		/*
-		if(frame_time > 50) {
-	 		if(scale > min_small_text_scale) {
-				min_small_text_scale += 0.1;
-				console.log("min_small_text_scale", min_small_text_scale);
-			} else
-			{
-				min_small_text_scale += 0.1;
-				min_normal_text_scale += 0.1;
-				console.log("min_normal_text_scale", min_normal_text_scale);
-				console.log("min_small_text_scale", min_small_text_scale);
-			}
+	}
+
+	// state serialization
+	function serialize_state() {
+		return search_input.property("value");
+	}
+
+	function deserialize_state(data) {
+		search_input.property("value", data);
+		search_input.node().focus();
+		search_input.node().select();
+		do_search(data);
+	}
+
+	var prev_ser_state;
+	function update_url() {
+		prev_ser_state = serialize_state();
+		window.location.hash = encodeURIComponent(prev_ser_state);
+	};
+
+	function update_from_url() {
+		var data = decodeURIComponent(window.location.hash.substr(1));
+		if (data != prev_ser_state) {
+			prev_ser_state = data;
+			deserialize_state(data);
 		}
-		*/
 	}
 
 	d3.select(window).on("load", init);
